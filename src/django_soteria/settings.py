169e4c520de358575e8b3a5a695ed06de14eac71
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-03-30 12:40:43 -0400

    added cors for easy deployment

--- src/django_soteria/settings.py
+++ src/django_soteria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o+1cbscoy7z$a^g&h6hhfq=vzw9%c#64r7ffhnx-rapwanyzg&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'src.chat_endpoint',
+    'src.chat_stream',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
