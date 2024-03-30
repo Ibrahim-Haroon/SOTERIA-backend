00a629271e0be85540af5b9db3ab1af7541e1029
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-03-30 15:05:33 -0400

    temp service demo

---
+++ src/demo/service_demo.py
@@ -0,0 +1,31 @@
+from src.ai_linkage.conversational_ai import conv_ai
+from src.server_db.get_user_db import get_user_info
+
+
+def main() -> None:
+    """
+    @rtype: None
+    @return: 0 if successful
+    """
+    print("Welcome to the demo!")
+    user_name = input("What is your full name? ")
+    account_details, _ = get_user_info(user_name)
+    print(account_details)
+    while (True):
+        user_response = input("What would you like to know about your account? ")
+        conversation_history = ""
+
+        soteria_response = conv_ai(
+            user_request=user_response,
+            user_account_details=account_details,
+            conversation_history=conversation_history,
+        )
+
+        print(soteria_response)
+        conversation_history += soteria_response
+
+    return
+
+
+if __name__ == "__main__":
+    main()
