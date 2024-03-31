d57738ab668ca94d4050769b4c355e827d2d29a5
Ibrahim Haroon <99413953+Ibrahim-Haroon@users.noreply.github.com>
2024-03-30 22:00:23 -0400

    Fixed issue of conversation history being wiped each time

--- src/demo/service_demo.py
+++ src/demo/service_demo.py
@@ -7,13 +7,13 @@ def main() -> None:
     @rtype: None
     @return: 0 if successful
     """
+    conversation_history = ""
     print("Welcome to the demo!")
     user_name = input("What is your full name? ")
     account_details, _ = get_user_info(user_name)
 
     while (True):
         user_response = input("What would you like to know about your account? ")
-        conversation_history = ""
 
         soteria_response = conv_ai(
             user_request=user_response,
